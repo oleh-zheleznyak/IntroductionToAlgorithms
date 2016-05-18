de4069263a9298cdc480b3d995e0525e6dac7d41
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-05-18 17:59:36 +0200

    GraphAlgorithms - iteration zero

---
+++ IntroductionToAlgorithms/GraphAlgorithms/DirectedGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms.GraphAlgorithms
+{
+    public class DirectedGraph<T>
+        where T : IEquatable<T>
+    {
+        public IEnumerable<T> Vertices { get { return _adjacencyList.Keys; } }
+
+        // TODO: refactor to get rid of tuple, introduce pair class instead
+        public IEnumerable<Tuple<T, T>> Edges { get { return _adjacencyList.SelectMany(x => x.Value.Select(y => Tuple.Create<T, T>(x.Key, y))); } }
+
+        private readonly Dictionary<T, HashSet<T>> _adjacencyList;
+    }
+}
