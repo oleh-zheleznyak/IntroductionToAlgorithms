b765ce150c1d4a16c2c82d35c851bfe5ea2fd2bb
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-05-22 20:59:28 +0200

    Added topological sort and test

---
+++ IntroductionToAlgorithms.Tests/GraphAlgorithms/TopologicalSortTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroductionToAlgorithms.GraphAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntroductionToAlgorithms.Tests.GraphAlgorithms;
+
+namespace IntroductionToAlgorithms.GraphAlgorithms.Tests
+{
+    [TestClass]
+    public class TopologicalSortTests
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            var graph = TestHelper.CreateSampleGraph();
+
+            var sut = new TopologicalSort<int>();
+
+            var iterator = new DepthFirstSearchIterator<int>(graph, 0);
+
+            var actual = sut.Sort(iterator);
+
+            var sortedVertices = actual.Select(x => x.Vertice).ToList();
+
+            var expected = new int[] { 0, 1, 3, 2, 4 };
+
+            CollectionAssert.AreEqual(expected, sortedVertices);
+        }
+    }
+}
---
+++ IntroductionToAlgorithms/GraphAlgorithms/TopologicalSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms.GraphAlgorithms
+{
+    public class TopologicalSort<T>
+        where T : IEquatable<T>
+    {
+        public IEnumerable<DfsVertice<T>> Sort(DepthFirstSearchIterator<T> dfs)
+        {
+            return dfs.Reverse();
+        }
+    }
+}
