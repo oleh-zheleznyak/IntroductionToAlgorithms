5d0fadf78f543e4627580919a92ebe36f3ba3f52
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-10-30 21:04:47 +0100

    Added a queue implemented as two stacks

---
+++ IntroductionToAlgorithms.Tests/DataStructures/TwoStackQueueTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroductionToAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms.DataStructures.Tests
+{
+    [TestClass]
+    public class TwoStackQueueTests
+    {
+        private TwoStackQueue<int> queue;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            queue = new TwoStackQueue<int>();
+        }
+
+        [TestMethod]
+        public void Enqueue_SavesItems()
+        {
+            var input = new[] { 1, 2, 3 };
+
+            input.ToList().ForEach(x => queue.Enqueue(x));
+
+            CollectionAssert.AreEquivalent(input, queue.ToList());
+        }
+
+        [TestMethod]
+        public void Dequeue_ReturnsItemsInCorrectOrder()
+        {
+            var input = new[] { 1, 2, 3 };
+
+            input.ToList().ForEach(x => queue.Enqueue(x));
+
+            var output = new List<int>(3);
+
+            while (queue.Count > 0)
+                output.Add(queue.Dequeue());
+
+            CollectionAssert.AreEqual(input, output);
+        }
+
+        [TestMethod]
+        public void TwoStackQueue_BehavesLikeAQueue()
+        {
+            var nativeQueue = new Queue<int>();
+            var dequeueOutput = new List<Tuple<int, int>>();
+
+            Action<int> enq = x => { queue.Enqueue(x); nativeQueue.Enqueue(x); };
+            Action deq = () => { dequeueOutput.Add(Tuple.Create(queue.Dequeue(), nativeQueue.Dequeue())); };
+
+            enq(1);
+            enq(2);
+            enq(3);
+            deq();
+            enq(4);
+            enq(5);
+            deq();
+
+            CollectionAssert.AreEquivalent(nativeQueue.ToList(), queue.ToList());
+            Assert.IsTrue(dequeueOutput.All(x => x.Item1 == x.Item2));
+        }
+    }
+}
---
+++ IntroductionToAlgorithms/DataStructures/TwoStackQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms.DataStructures
+{
+    /// <summary>
+    /// Queue implementation using two stacks, excersise 10.1-6
+    /// </summary>
+    /// <remarks>
+    /// This implementation is not efficient, provided only to complete the excersise
+    /// Array/LinkedList based queue will provide constant enq and deq time, but this implementation
+    /// will have both of those operations linear in the worst case (if used in turn)
+    /// Use <see cref="Queue{T}"/> instead of this class
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    public class TwoStackQueue<T> : IQueue<T>, IEnumerable<T>
+    {
+        private readonly Stack<T> forward = new Stack<T>();
+        private readonly Stack<T> backward = new Stack<T>();
+
+        public T Dequeue()
+        {
+            if (Count == 0) throw new InvalidOperationException("Queue is empty");
+
+            Reverse(forward, backward);
+
+            return backward.Pop();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Math.Max(forward.Count, backward.Count); // only one of them is not zero
+            }
+        }
+
+        public void Enqueue(T t)
+        {
+            Reverse(backward, forward);
+
+            forward.Push(t);
+        }
+
+        private void Reverse(Stack<T> from, Stack<T> to)
+        {
+            if (to.Count > 0 && from.Count > 0) throw new InvalidOperationException();
+
+            while (from.Count > 0)
+                to.Push(from.Pop());
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Reverse(backward, forward);
+
+            return forward.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
