e2f8ee7900b413b672360c9015b7f8cfba3dcac5
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-11-06 21:27:52 +0100

    singly linked list - first take

---
+++ IntroductionToAlgorithms/DataStructures/SinglyLinkendList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms.DataStructures
+{
+    public class SinglyLinkendList<T> : IEnumerable<T>, IReadOnlyCollection<T>
+    {
+        private int count;
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public Node Head { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            Head = new Node(value, Head);
+            count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (Head == null) throw new InvalidOperationException("linked list is empty");
+
+            var result = Head;
+            Head = Head.Next;
+            count--;
+
+            return result.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public class Node
+        {
+            public Node(T value, Node next)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public T Value { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}
